f094df2fcd9536e19107acb2055fa30975d83a37
Samuel Feijó <samuelfeijo@lampp-it.com.br>
2024-05-21 13:27:53 -0500

    implementando fila

--- app/Mail/Contact.php
+++ app/Mail/Contact.php
@@ -10,7 +10,7 @@ use Illuminate\Mail\Mailables\Content;
 use Illuminate\Mail\Mailables\Envelope;
 use Illuminate\Queue\SerializesModels;
 
-class Contact extends Mailable
+class Contact extends Mailable implements ShouldQueue
 {
     use Queueable, SerializesModels;
 
