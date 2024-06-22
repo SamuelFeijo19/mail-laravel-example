92db307cd9b6949e25ffe4ea587f65890e5456ce
Samuel Feijó <112981613+SamuelFeijo19@users.noreply.github.com>
2024-06-21 19:06:34 -0500

    alterando rota raiz do projeto

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,5 @@
 use App\Http\Controllers\ContactController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/contact', [ContactController::class, 'index'])->name('contact.index');
+Route::get('/', [ContactController::class, 'index'])->name('contact.index');
 Route::post('/contact', [ContactController::class, 'store'])->name('contact.store');
